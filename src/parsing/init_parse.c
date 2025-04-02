4beec93ce3487e4ffe895a4a2c97e1921ea9d57d
Gaston Roman <113539513+GastonRLdL@users.noreply.github.com>
2025-04-02 15:22:48 +0000

    steing it up

--- src/parsing/init_parse.c
+++ src/parsing/init_parse.c
@@ -1,23 +1,41 @@
-void parse_data(int fd)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_parse.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasroman <gasroman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 14:23:07 by gasroman          #+#    #+#             */
+/*   Updated: 2025/04/02 14:32:46 by gasroman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	parse_data(int fd)
 {
 	char	*line;
 	int		stored;
 
 	stored = 0;
-	while ((line = get_next_line(fd)))
+	while (line = get_next_line(fd))
 	{
-		trim(line);
-		if (is_texture(line))
-			store_texture(line);
-		else if (is_color(line))
-			store_color(line);
-		else
+		if (stored < 6)
 		{
+			ft_trim(line);
+			if (is_texture(line))
+				store_texture(line);
+			else if (is_color(line))
+				store_color(line);
+			else
+			{
+				free(line);
+				printf("Error en datos\n");
+				return;
+			}
+			stored++;
 			free(line);
-			printf("Error en datos\n");
-			return;
 		}
-		stored++;
+		else if (stored == 6)
+			parse_map();
 		free(line);
 	}
 }
--- src/parsing/is_color.c
+++ src/parsing/is_color.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   is_color.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gasroman <gasroman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/02 14:18:54 by gasroman          #+#    #+#             */
-/*   Updated: 2025/04/02 14:22:59 by gasroman         ###   ########.fr       */
+/*   Created: 2025/04/02 14:34:15 by gasroman          #+#    #+#             */
+/*   Updated: 2025/04/02 14:34:16 by gasroman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-
-int main (int ac, char **av, char *env)
-{
-	int	fd;
-
-	if(ac =! 1)
-		return(print_error(1));
-	fd = open(av[1], O_RDONLY);
-	if(fd == -1)
-		return(print_error(1));
-	parse_data(fd);
-	close(fd);
-	return(0);
-}
